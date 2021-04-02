e62aab3b626de593176d9ea77742bfe819c38350
Gildardo <gildardodiaz6937@gmail.com>
2021-04-01 19:28:42 -0700

    Primer ajuste

---
+++ carrito/src/App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <Footer/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ carrito/src/componentes/Header.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Header(){
+    
+    
+    return(
+        <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
+    )
+}
+
+export default Header;
